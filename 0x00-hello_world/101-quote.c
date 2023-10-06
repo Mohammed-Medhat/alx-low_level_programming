6fa9883da2839b51e0081d10b4ec54311f494645
mohammed medhat <mamadomedhat10.com>
2023-10-05 19:11:58 -0700

    F

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+/**
+ *main - Entry point
+ *Description:C program that prints without any function listed
+ *Return: Alawys 1 (Success)
+*/
+int main(void)
+{
+char quol[] = "and that piece of art is useful\" - Dora Korpar, 2015-11
+write(1,quo, 59);
+return (1);
+}
