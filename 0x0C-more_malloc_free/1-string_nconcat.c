f32596674e5cfa074315cd09d35754adaea32dae
mohammed medhat <mamadomedhat10.com>
2023-11-02 08:51:47 -0700

    G

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+*string_nconcat - Concatenates two strings using at
+*most an inputted number of bytes.
+*@s1: The first string.
+*@s2: The second string.
+*@n: The maximum number of bytes of s2 to concatenate to s1.
+*
+*Return: If the function fails - NULL.
+*Otherwise - a pointer to the concatenated space in memory.
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n){
+    char *i;
+    if (s1==NULL){
+        s1="";
+    }
+    if(s2==NULL){
+        s2="";
+    }
+    i=malloc(sizeof(s1)+sizeof(s2)+1);
+    if (i=NULL){
+        return(NULL);
+    }
+    for (int f=0;f<sizeof(s1);f++){
+        i[f]=s1[f];
+
+    }
+     for (int f=0;f<sizeof(s2);f++){
+        i[f+sizeof(s1)]=s2[f];
+        i[sizeof(s1)+sizeof(s2)]='\0';
+
+    }
+    return(i);
+
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,6 +8,7 @@ int check_for_digits(char **av);
 void init(char *str, int l);
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
