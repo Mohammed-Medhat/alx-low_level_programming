058360479903dcb116ff3067b81ee26b41e130e1
mohammed medhat <mamadomedhat10.com>
2023-11-02 08:57:29 -0700

    G

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,26 +11,30 @@
 *Otherwise - a pointer to the concatenated space in memory.
 */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n){
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
     char *i;
-    if (s1==NULL){
-        s1="";
+    if (s1 == NULL)
+    {
+        s1 = "";
     }
-    if(s2==NULL){
-        s2="";
+    if(s2 == NULL)
+    {
+        s2 = "";
     }
-    i=malloc(sizeof(s1)+sizeof(s2)+1);
-    if (i=NULL){
+    i = malloc(sizeof(s1)+sizeof(s2)+1);
+    if (i = NULL)
+    {
         return(NULL);
     }
-    for (int f=0;f<sizeof(s1);f++){
-        i[f]=s1[f];
-
+    for (int f = 0; f<sizeof(s1); f++)
+    {
+        i[f] = s1[f];
     }
-     for (int f=0;f<sizeof(s2);f++){
-        i[f+sizeof(s1)]=s2[f];
-        i[sizeof(s1)+sizeof(s2)]='\0';
-
+     for (int f = 0; f<sizeof(s2); f++)
+     {
+        i[f+sizeof(s1)] = s2[f];
+        i[sizeof(s1)+sizeof(s2)] = '\0';
     }
     return(i);
 
